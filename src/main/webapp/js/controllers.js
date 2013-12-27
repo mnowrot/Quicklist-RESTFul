af1f63e07897560a31269088a283cc974e08247c
mnowrot <marcin.nowrot@gmail.com>
2013-12-27 22:57:03 +0100

    Basic AngularJS functionality

---
+++ src/main/webapp/js/controllers.js
@@ -0,0 +1,23 @@
+var quicklistApp = angular.module('quicklistApp', []);
+
+quicklistApp.controller('ListManager', function($scope, $http) {
+	reloadList($scope, $http);
+	
+	$scope.addItem = function() {
+		var newItemName = $scope.newItemName;
+		if(newItemName && newItemName.length > 0) {
+			$http.post('qlist/add', 'listItemName=' + newItemName, 
+					{headers: {'Content-Type' : 'application/x-www-form-urlencoded'}})
+			.success(function(data) {
+				$scope.newItemName = '';
+				reloadList($scope, $http);
+			});
+		}
+	};
+});
+
+function reloadList($scope, $http) {
+	$http.get('qlist/all').success(function(data) {
+		$scope.items = data;
+	});
+}
