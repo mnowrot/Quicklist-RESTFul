23a83a10f6e8f34b30d6383a5e3396b71fe206fe
mnowrot <marcin.nowrot@gmail.com>
2014-01-20 19:38:57 +0100

    Core committed

---
+++ core/src/main/java/org/mnowrot/quicklist/service/QuicklistService.java
@@ -0,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mnowrot.quicklist.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.mnowrot.quicklist.model.ListItem;
+
+/**
+ * Service that makes quicklist operations available
+ * 
+ * @author PLMANOW4
+ * 
+ */
+@Stateless
+public class QuicklistService {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public List<ListItem> getAllItems() {
+		return em.createNamedQuery("findAll", ListItem.class).getResultList();
+	}
+	
+	public ListItem getItemById(Long listItemId) {
+		return em.find(ListItem.class, listItemId);
+	}
+
+	public ListItem addItem(String newItemName) {
+		ListItem newItem = new ListItem();
+		newItem.setName(newItemName);
+		em.persist(newItem);
+		return newItem;
+	}
+
+	public void removeItem(Long listItemId) {
+		ListItem toRemove = getItemById(listItemId);
+		em.remove(toRemove);
+	}
+
+	public void editItemName(Long listItemId, String editedItemName) {
+		ListItem toEdit = getItemById(listItemId);
+		toEdit.setName(editedItemName);
+	}
+}
---
+++ core/src/test/java/org/mnowrot/quicklist/service/QuicklistServiceTest.java
@@ -0,0 +1,115 @@
+package org.mnowrot.quicklist.service;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mnowrot.quicklist.model.ListItem;
+
+/**
+ * 
+ * 
+ * @author PLMANOW4
+ * 
+ */
+@RunWith(Arquillian.class)
+public class QuicklistServiceTest {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Inject
+	private UserTransaction utx;
+
+	@EJB
+	private QuicklistService quicklistService;
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		return ShrinkWrap
+				.create(WebArchive.class,
+						QuicklistServiceTest.class.getSimpleName() + ".war")
+				.addClass(QuicklistService.class)
+				.addClass(ListItem.class)
+				.addAsResource("META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				.addAsLibraries(
+						Maven.resolver()
+								.resolve("org.easytesting:fest-assert:1.4")
+								.withTransitivity().asFile());
+	}
+
+	@Test
+	public void listPersistenceTest() throws Exception {
+		// given
+		utx.begin();
+		em.joinTransaction();
+
+		//when
+		quicklistService.addItem("TestListItemName1");
+		quicklistService.addItem("TestListItemName2");
+		final List<ListItem> allItems = quicklistService.getAllItems();
+
+		// then
+		assertThat(allItems).isNotNull();
+		assertThat(allItems).isNotEmpty();
+		assertThat(allItems).hasSize(2);
+		utx.rollback();
+
+	}
+	
+	@Test
+	public void listDeletionTest() throws Exception {
+		// given
+		utx.begin();
+		em.joinTransaction();
+		ListItem toDelete = quicklistService.addItem("TestListItemName1");
+		quicklistService.addItem("TestListItemName2");
+
+		//when
+		quicklistService.removeItem(toDelete.getId());
+		final List<ListItem> allItems = quicklistService.getAllItems();
+
+		// then
+		assertThat(allItems).isNotNull();
+		assertThat(allItems).isNotEmpty();
+		assertThat(allItems).hasSize(1);
+		utx.rollback();
+	}
+	
+	@Test
+	public void listEditionTest() throws Exception {
+		// given
+		utx.begin();
+		em.joinTransaction();
+		ListItem toEdit = quicklistService.addItem("TestListItemName1");
+		quicklistService.addItem("TestListItemName2");
+
+		//when
+		quicklistService.editItemName(toEdit.getId(), "TestListItemName3");
+		final List<ListItem> allItems = quicklistService.getAllItems();
+		final ListItem edited = quicklistService.getItemById(toEdit.getId());
+
+		// then
+		assertThat(allItems).isNotNull();
+		assertThat(allItems).isNotEmpty();
+		assertThat(allItems).hasSize(2);
+		assertThat(edited).isNotNull();
+		assertThat(edited.getName()).isEqualTo("TestListItemName3");
+		utx.rollback();
+	}
+}
