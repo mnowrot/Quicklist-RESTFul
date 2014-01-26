8134d8f0d37617ba5571771d11ce69ad3209f4f6
mnowrot <marcin.nowrot@gmail.com>
2014-01-26 23:34:33 +0100

    Failing test, refactoring needed

--- testwebdriver/src/test/java/org/mnowrot/quicklist/webdrivertests/IndexPageTest.java
+++ testwebdriver/src/test/java/org/mnowrot/quicklist/webdrivertests/IndexPageTest.java
@@ -5,9 +5,12 @@ package org.mnowrot.quicklist.webdrivertests;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.util.List;
+
 import org.mnowrot.quicklist.webdrivertests.config.BrowserAndUrlProvider;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 /**
@@ -30,6 +33,25 @@ public class IndexPageTest {
 	
 	@Test(dataProvider = "browserAndUrlProvider", dataProviderClass = BrowserAndUrlProvider.class, 
 			dependsOnMethods = {"shouldShowOneRowInEmptyTableTest"})
+	public void shouldAddListItemOnClickTest(WebDriver browser, String url) {
+		// given
+		String itemName = "FirstListItem";
+		WebElement newListItemInput = browser.findElement(By.id("newListItemInput"));
+		WebElement addNewListItemButton = browser.findElement(By.id("addNewListItemButton"));
+		
+		// when
+		newListItemInput.sendKeys(itemName);
+		addNewListItemButton.click();
+		
+		// then
+		List<WebElement> newListItemCells = browser.findElement(By.id("listItemsTable")).findElements(By.xpath("tbody/tr/td"));
+		assertThat(newListItemCells).isNotNull();
+		assertThat(newListItemCells).hasSize(3);
+		assertThat(newListItemCells.get(1).getText()).isEqualTo(itemName);
+	}
+	
+	@Test(dataProvider = "browserAndUrlProvider", dataProviderClass = BrowserAndUrlProvider.class, 
+			dependsOnMethods = {"shouldAddListItemOnClickTest"})
 	public void shutdown(WebDriver browser, String url) {
 		browser.quit();
 	}
