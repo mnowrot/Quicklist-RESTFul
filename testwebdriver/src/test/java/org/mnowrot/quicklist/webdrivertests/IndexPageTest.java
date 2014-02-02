fd5969e0b95b5b54f7bb304a014658a03a730eef
mnowrot <marcin.nowrot@gmail.com>
2014-02-03 00:01:48 +0100

    Resetting the list state after each test

--- testwebdriver/src/test/java/org/mnowrot/quicklist/webdrivertests/IndexPageTest.java
+++ testwebdriver/src/test/java/org/mnowrot/quicklist/webdrivertests/IndexPageTest.java
@@ -11,6 +11,7 @@ import org.mnowrot.quicklist.webdrivertests.config.BrowserProvider;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -18,55 +19,67 @@ import org.testng.annotations.Test;
 
 /**
  * @author PLMANOW4
- *
+ * 
  */
 public class IndexPageTest {
-	
+
 	private String url;
 	private WebDriver browser;
-	
+
 	@BeforeTest
-	@Parameters({"url"})
+	@Parameters({ "url" })
 	public void setUpUrl(String url) {
 		this.url = url;
 	}
-	
+
 	@BeforeTest
-	@Parameters({"browser"})
+	@Parameters({ "browser" })
 	public void setUpBrowser(String browserName) {
 		this.browser = BrowserProvider.provideBrowserByName(browserName);
 	}
-	
+
 	@Test
 	public void shouldShowOneRowInEmptyTableTest() {
 		// given
 		browser.get(url);
-		
+
 		// when
 		int tableSize = browser.findElement(By.id("listItemsTable")).findElements(By.xpath("tbody/tr")).size();
-		
+
 		// then
 		assertThat(tableSize).isEqualTo(1);
 	}
-	
-	@Test(dependsOnMethods = {"shouldShowOneRowInEmptyTableTest"})
+
+	@Test(dependsOnMethods = { "shouldShowOneRowInEmptyTableTest" })
 	public void shouldAddListItemOnClickTest() {
 		// given
 		String itemName = "FirstListItem";
 		WebElement newListItemInput = browser.findElement(By.id("newListItemInput"));
 		WebElement addNewListItemButton = browser.findElement(By.id("addNewListItemButton"));
-		
+
 		// when
 		newListItemInput.sendKeys(itemName);
 		addNewListItemButton.click();
-		
+
 		// then
 		List<WebElement> newListItemCells = browser.findElement(By.id("listItemsTable")).findElements(By.xpath("tbody/tr/td"));
 		assertThat(newListItemCells).isNotNull();
 		assertThat(newListItemCells).hasSize(4);
 		assertThat(newListItemCells.get(1).getText()).isEqualTo(itemName);
 	}
-	
+
+	@AfterTest
+	public void deleteAllItems() {
+		while (browser.findElements(By.id("removeItemButton")).size() > 0) {
+			WebElement removeButton = browser.findElement(By.id("removeItemButton"));
+			removeButton.click();
+			new Actions(browser).pause(1000).perform();
+			WebElement confirmRemovalButton = browser.findElement(By.id("confirmItemRemovalButton"));
+			confirmRemovalButton.click();
+			new Actions(browser).pause(1000).perform();
+		}
+	}
+
 	@AfterTest
 	public void shutdownBrowser() {
 		browser.quit();
