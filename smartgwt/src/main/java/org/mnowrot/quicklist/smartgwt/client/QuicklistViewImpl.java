58ae1fe5bf067389dc34934542cafd36460b5c1a
mnowrot <marcin.nowrot@gmail.com>
2014-11-22 21:54:39 +0100

    Showing first SmartGWT widget

--- smartgwt/src/main/java/org/mnowrot/quicklist/smartgwt/client/QuicklistViewImpl.java
+++ smartgwt/src/main/java/org/mnowrot/quicklist/smartgwt/client/QuicklistViewImpl.java
@@ -6,8 +6,11 @@ package org.mnowrot.quicklist.smartgwt.client;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Widget;
 import com.smartgwt.client.widgets.BaseWidget;
+import com.smartgwt.client.widgets.grid.ListGrid;
+import com.smartgwt.client.widgets.grid.ListGridField;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 /**
@@ -16,6 +19,8 @@ import com.smartgwt.client.widgets.layout.VLayout;
  */
 public class QuicklistViewImpl implements QuicklistView {
 	
+	Messages messages = GWT.create(Messages.class);
+	
 	List<QuicklistViewListener> listeners = new ArrayList<>();
 	
 	private Widget wrappingWidget;
@@ -23,10 +28,23 @@ public class QuicklistViewImpl implements QuicklistView {
 	public QuicklistViewImpl() {
 		super();
 		
+		ListGridField id = new ListGridField("ID#");
+		ListGridField name = new ListGridField("Name");
+		ListGridField options = new ListGridField("Options");
+		
+		ListGrid listGrid = new ListGrid();
+		listGrid.setCanRemoveRecords(true);		  
+		listGrid.setWidth(550);  
+		listGrid.setHeight(224);  
+		listGrid.setShowAllRecords(true);
+		listGrid.setEmptyMessage(messages.listEmpty());
+		listGrid.setFields(id, name, options);
 		
 		VLayout mainView = new VLayout(10);  
         mainView.setHeight100();  
         mainView.setWidth100();  
+        mainView.addMember(listGrid);
+        
         setWrappingWidget(mainView);
  	}
 
